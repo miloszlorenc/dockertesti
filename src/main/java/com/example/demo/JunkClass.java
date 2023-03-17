a44fa1796bf0e5bbf452e42731d7dc8250433939
miloszlorenc <60222801+miloszlorenc@users.noreply.github.com>
2023-03-17 12:29:39 +0100

    ABC-123 test #3 (#1)

---
+++ src/main/java/com/example/demo/JunkClass.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+public class JunkClass
+{
+    private long amount;
+
+    /**
+     * Sets the amount.
+     *
+     * @param amount the amount.
+     */
+    public void setAmount(final long amount)
+    {
+        this.amount = amount;
+    }
+
+    /**
+     * Returns the amount.
+     *
+     * @return the amount.
+     */
+    public long getAmount()
+    {
+        return amount;
+    }
+}
